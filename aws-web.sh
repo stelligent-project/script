a4ea05536aaa511eb154949b5b4706dd92896dd9
peacehaven <sunny@peacehaven.co>
2016-05-06 13:22:02 -0500

    Added files via upload

---
+++ aws-web.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+aws ec2 run-instances --image-id ami-775e4f16  --count 1 --instance-type t2.micro --key-name peacehavencorp --security-groups default --query 'Instances[0].InstanceId' --user-data file:///C:/Users/UC191564/Dropbox/http.sh
---
+++ http.sh
@@ -0,0 +1,12 @@
+#!/bin/bashyum update -y
+yum install -y httpd24 php56 mysql55-server php56-mysqlnd
+service httpd start
+chkconfig httpd on
+groupadd www
+usermod -a -G www ec2-user
+chown -R root:www /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} +
+find /var/www -type f -exec chmod 0664 {} +
+cd /var/www/html
+echo "Automation for the People" > index.html
